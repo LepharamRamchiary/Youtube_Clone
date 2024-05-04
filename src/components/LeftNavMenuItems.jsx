db87b4f998b89fdb1cac4957943c2b3575a3305d
Lepha <lepharamchiary@gmail.com>
2024-05-04 09:56:17 +0530

    add new

--- src/components/LeftNavMenuItems.jsx
+++ src/components/LeftNavMenuItems.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const LeftNavMenuItems = () => {
+const LeftNavMenuItems = ({text, icon, className, action}) => {
   return (
-    <div>LeftNavMenuItems</div>
+    <div className={"text-white text-sm cursor-pointer h-10 flex items-center px-3 mb-[1px] rounded-lg hover:bg-white/[0.15] " +
+    className}>
+      <spam className="text-xl mr-5">{icon}</spam>
+      {text}
+    </div>
   )
 }
 
